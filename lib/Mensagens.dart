06e4b89c14c89854b20e41a5cdeec0597e1a0740
Apollo Daniel <developer.apollo.mail@gmail.com>
2022-01-19 11:45:51 -0300

    Messages working

--- lib/Mensagens.dart
+++ lib/Mensagens.dart
@@ -18,12 +18,14 @@ class Mensagens extends StatefulWidget {
 class _MensagensState extends State<Mensagens> {
   TextEditingController _mensagensController = TextEditingController();
 
-  List<Message> _mensagens = [];
-
-
   late FirebaseAuth mAuth;
   late FirebaseFirestore firestore;
   late FirebaseStorage storage;
+
+  var message_widget;
+  List<Message> messages = [];
+
+  String id_chat = "";
 
   @override
   void initState() {
@@ -32,91 +34,37 @@ class _MensagensState extends State<Mensagens> {
     mAuth = FirebaseAuth.instance;
     firestore = FirebaseFirestore.instance;
     storage = FirebaseStorage.instance;
-
-
+    getMessage();
   }
 
-  _recuperaMensagens()async{
+  getMessage() async{
+    firestore
+        .collection("mensagens")
+        .snapshots().listen((querySnapshot)async {
 
+      for(DocumentSnapshot doc in querySnapshot.docs) {
+        if (doc.get("users").contains(mAuth.currentUser!.uid) &&
+            doc.get("users").contains(widget.pessoa.id)) {
+          QuerySnapshot event = await doc.reference.collection("mensagens").get();
+          List<Message> mensagens = event.docs.map(
+                  (event) {
+                return Message(content: event.get("content"),
+                    date: event.get("date"),
+                    sender: event.get("sender"));
+              }
+          ).toList();
+          id_chat = doc.id;
+          setState(() {
+            messages.clear();
+            messages.addAll(mensagens);
+          });
+        }
+    }});
   }
+
 
   @override
   Widget build(BuildContext context) {
-    _recuperaMensagens();
-
-
-
-    StreamBuilder streamBuilder = StreamBuilder(
-      stream: firestore.collection("mensagens").snapshots(),
-      builder: (context, snapshot) {
-        switch(snapshot.connectionState){
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-            return Center(
-              child: Column(
-                children: [
-                  Text("Carregando mensagens"),
-                  CircularProgressIndicator()
-                ],
-              ),
-            );
-            break;
-          case ConnectionState.active:
-          case ConnectionState.done:
-            if(snapshot.hasError){
-              return Center(
-                child: Text("Um erro ocorreu"),
-              );
-            }else{
-              QuerySnapshot querySnapshot = snapshot.data;
-
-              for(DocumentSnapshot doc in querySnapshot.docs){
-                print(doc.id.toString());
-              }
-              List<Message> messages = []; /*querySnapshot.docs.map(
-                      (e) {
-                        return Message(content: e.get("content"), date: e.get("date"), sender: e.get("sender"));
-              }).toList();*/
-
-              return Expanded(
-                  child: ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-
-                      Alignment alignment = Alignment.centerRight;
-                      Color cor = Colors.green;
-                      double larguraContainer = MediaQuery.of(context).size.width *  80 / 100;
-
-                      if(messages[index].sender == widget.pessoa.id){
-                        alignment = Alignment.centerLeft;
-                        cor = ThemeData.dark().primaryColor;
-                      }
-
-
-                      return Align(
-                        alignment: alignment,
-                        child: Padding(
-                          padding: EdgeInsets.all(6),
-                          child: Container(
-                            width: larguraContainer,
-                            decoration: BoxDecoration(
-                                color: cor,
-                                borderRadius: BorderRadius.all(Radius.circular(8))
-                            ),
-                            padding: EdgeInsets.all(16),
-                            child: Text(messages[index].content),
-                          ),
-                        ),
-                      );
-                    },
-                  )
-              );
-            }
-            break;
-        }
-      },
-    );
 
     return Scaffold(
         appBar: AppBar(
@@ -133,7 +81,10 @@ class _MensagensState extends State<Mensagens> {
           ),
         ),
         body: Container(
-            width: MediaQuery.of(context).size.width,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
             decoration: BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage("images/bg.jpg"),
@@ -141,75 +92,125 @@ class _MensagensState extends State<Mensagens> {
                     fit: BoxFit.cover)),
             child: SafeArea(
               child: Container(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      streamBuilder,
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.only(right: 8),
-                                child: TextField(
-                                  controller: _mensagensController,
-                                  autofocus: true,
-                                  keyboardType: TextInputType.text,
-                                  style: TextStyle(fontSize: 20),
-                                  decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: ThemeData.dark().primaryColor,
-                                      contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                                      hintText: "Digite uma mensagem...",
-                                      border: OutlineInputBorder(
-                                          borderRadius:
-                                          BorderRadius.circular(32)),
-                                      prefixIcon: IconButton(
-                                          icon: Icon(Icons.camera_alt),
-                                          onPressed: () {})),
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                  Expanded(
+                  child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  Alignment alignment = Alignment.centerRight;
+                  Color cor = Colors.green;
+                  double larguraContainer = MediaQuery
+                      .of(context)
+                      .size
+                      .width * 80 / 100;
+
+                  if (messages[index].sender == widget.pessoa.id) {
+                    alignment = Alignment.centerLeft;
+                    cor = ThemeData
+                        .dark()
+                        .primaryColor;
+                  }
+
+
+                  return Align(
+                    alignment: alignment,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: Container(
+                        width: larguraContainer,
+                        decoration: BoxDecoration(
+                            color: cor,
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(8))
+                        ),
+                        padding: EdgeInsets.all(16),
+                        child: Text(messages[index].content),
+                      ),
+                    ),
+                  );
+                },
+              )
+            ),
+                        Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: 8),
+                                  child: TextField(
+                                    controller: _mensagensController,
+                                    autofocus: true,
+                                    keyboardType: TextInputType.text,
+                                    style: TextStyle(fontSize: 20),
+                                    decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor: ThemeData
+                                            .dark()
+                                            .primaryColor,
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            32, 8, 32, 8),
+                                        hintText: "Digite uma mensagem...",
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(32)),
+                                        prefixIcon: IconButton(
+                                            icon: Icon(Icons.camera_alt),
+                                            onPressed: () {})),
+                                  ),
                                 ),
                               ),
-                            ),
-                            FloatingActionButton(
-                              backgroundColor: Color(0xff075E54),
-                              child: Icon(
-                                Icons.send,
-                                color: Colors.white,
-                              ),
-                              mini: true,
-                              onPressed: () => _salvarMensagem(),
-                            )
-                          ],
+                              FloatingActionButton(
+                                backgroundColor: Color(0xff075E54),
+                                child: Icon(
+                                  Icons.send,
+                                  color: Colors.white,
+                                ),
+                                mini: true,
+                                onPressed: _salvarMensagem,
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    ])
+                      ])
               ),
-            )));
+            )
+        )
+    );
   }
 
-  _salvarMensagem()async{
+    _salvarMensagem() async {
+      String mensagem = _mensagensController.text;
 
-    String mensagem = _mensagensController.text;
+      String _chatId = id_chat;
 
-    User user = mAuth.currentUser!;
-    if(mensagem.isNotEmpty)
-    {
-      Message message = Message(content: mensagem, date: DateTime.now().toString(), sender: mAuth.currentUser!.uid);
-      await firestore
-          .collection("mensagens")
-          .add({
-        "users":[
-          widget.pessoa.id,
-          mAuth.currentUser!.uid
-        ],
-      }).then(
-              (value) => {
-                value.collection("mensagens").add(message.toMap())
-              });
-      _mensagensController.clear();
-    }
+      if(id_chat.isEmpty){
+        _chatId = widget.pessoa.id + "||" + mAuth.currentUser!.uid;
+      }
+
+      if (mensagem.isNotEmpty) {
+        Message message = Message(content: mensagem,
+            date: DateTime.now().toString(),
+            sender: mAuth.currentUser!.uid);
+        await firestore
+            .collection("mensagens")
+            .doc(_chatId).set({
+          "users": [
+            widget.pessoa.id,
+            mAuth.currentUser!.uid
+          ],
+        }).whenComplete(() {
+          firestore
+              .collection("mensagens")
+              .doc(_chatId).collection("mensagens").add(message.toMap());
+
+        });
+        _mensagensController.clear();
+        getMessage();
+      }
   }
-
 }
